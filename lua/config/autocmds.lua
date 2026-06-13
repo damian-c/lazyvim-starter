be561f2d8928b62c84002f132a8bb14848eae88f
Damian Christey <damian.christey@gmail.com>
2026-06-12 23:26:03 -0400

    Auto reload when files change, enable more extras that might be useful

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,8 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Reload files when they change on disk
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  command = "checktime",
+})
